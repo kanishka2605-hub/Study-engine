866f7efec742c2d0c7f261883421e940f801a6ba
kanishka2605-hub <kanishkag.26300611@kalvium.community>
2026-09-10 10:15:10 +0530

    basics_of_js

---
+++ notes.js
@@ -0,0 +1,34 @@
+// // if(2==2){
+// //     var  name="rohit";
+// //     console.log("inside the block",name)
+// // }
+// // console.log("outside block",name);
+
+// let name='adbasdvksd'
+// function isEven(num){
+
+//     var name="monik"
+//     if(num%2==0){
+//         console.log("even")
+//     }
+//     else{
+//         console.log('odd')
+//     }
+//     console.log(name , "in")
+// }
+// console.log(name,"out")
+
+// isEven(4)
+// // isEven(5)
+// console.log(name)
+// var name="rohit";
+
+
+// function fact(n){
+//     if(n==1){
+//         return 1;
+//     }
+//     return n*fact(n-1)
+// }
+
+// console.log(fact(5))
---
+++ notes2.js
@@ -0,0 +1,36 @@
+// let arr=[1,2,3,4,5];
+// arr.push(6);
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// arr.unshift(0);
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+// // slice , splice
+// // let ans=arr.slice(0,3);
+// // console.log(ans);
+// arr.splice(2,0,8);
+// console.log(arr)
+
+// Objects
+// let obj={
+//   a:1,
+//   b:2,
+//   c:3
+// }
+// obj.d=4
+// obj.a=4;
+// delete(obj.c)
+// console.log(obj),;
+// let arr=["a","b","c","a","b","d","@","&","@","&"];
+// let obj={}
+// for(let i=0;i<arr.length;i++){
+//   if(obj[arr[i]]==undefined){
+//     obj[arr[i]]=1;
+//   }
+//   else{
+//       obj[arr[i]]+=1;
+//   }
+// }
+// console.log(obj)
