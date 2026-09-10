903849647de7cda92a318567de35db84ac144a02
kanishka2605-hub <kanishkag.26300611@kalvium.community>
2026-09-10 10:51:40 +0530

    basics

---
+++ notes3.js
@@ -0,0 +1,40 @@
+// ES6 modern javascript
+
+// Normal 
+// function greet(){
+//     return "Don't touch my laptop";
+// }
+// greet()
+
+// Arrow functionlet warn=()=>{
+//     console.log("Be careful monik")
+// }
+// warn()
+
+// Tempelate literals
+// let name= "Rohit";
+// let std="12th";
+// console.log(`My name is ${greet()} and I am studying in ${std}`)
+
+// Destructuring
+// let students = ["kanishka", "anishee", "riddhima", "parul"];
+// let [a,,c]= students;
+// console.log(c)
+
+// let obj={
+//     name:"mahesh"
+//     std="nursery"
+// }
+// console.log(name)
+
+// spread and rest
+// console.log(Math.max(1,2,3,4,5,6))
+// let arr=[1,2,3,4,5,6]
+//     console.log(Math.max(...arr))
+
+// Rest operators
+// function sum(...rest){
+//     console.log(rest)
+// }    
+
+// sum(1,2,3,4,5,6,7,8)
